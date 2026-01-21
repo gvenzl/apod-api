4efbc2baaf555617b19a485317bfdb6508b05cad
Daniel C. Rendon <daniel.rendonfc@gmail.com>
2026-01-21 13:23:14 -0600

    updating docker setup

--- application.py
+++ application.py
@@ -380,7 +380,4 @@ def app_error(e):
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=8000)
-
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=8000)
+    app.run("0.0.0.0", port=5000, debug=True)
