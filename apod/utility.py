ed607f7dd709a45c9498a9a8c44796647eb9f1b3
Daniel C. Rendon <daniel.rendonfc@gmail.com>
2026-01-21 12:33:56 -0600

    formatting

--- apod/utility.py
+++ apod/utility.py
@@ -6,32 +6,41 @@ Created on Mar 24, 2017
 @author=bathomas @email=brian.a.thomas@nasa.gov
 """
 
-from bs4 import BeautifulSoup
 import datetime
-import requests
-import logging
 import json
+import logging
 import re
+
+import requests
 import urllib3
+from bs4 import BeautifulSoup
+
 # import urllib.request
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARN)
 
 # location of backing APOD service
-BASE = 'https://apod.nasa.gov/apod/'
+BASE = "https://apod.nasa.gov/apod/"
 
 # Create urllib3 Pool Manager
 http = urllib3.PoolManager()
+
 
 # function for getting video thumbnails
 def _get_thumbs(data):
     global video_thumb
     if "youtube" in data or "youtu.be" in data:
         # get ID from YouTube URL
-        youtube_id_regex = re.compile("(?:(?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)")
+        youtube_id_regex = re.compile(
+            "(?:(?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)"
+        )
         video_id = youtube_id_regex.findall(data)
-        video_id = ''.join(''.join(elements) for elements in video_id).replace("?", "").replace("&", "")
+        video_id = (
+            "".join("".join(elements) for elements in video_id)
+            .replace("?", "")
+            .replace("&", "")
+        )
         # get URL of thumbnail
         video_thumb = "https://img.youtube.com/vi/" + video_id + "/0.jpg"
     elif "vimeo" in data:
@@ -39,30 +48,34 @@ def _get_thumbs(data):
         vimeo_id_regex = re.compile("(?:/video/)(\d+)")
         vimeo_id = vimeo_id_regex.findall(data)[0]
         # make an API call to get thumbnail URL
-        vimeo_request = http.request("GET", "https://vimeo.com/api/v2/video/" + vimeo_id + ".json")
-        data = json.loads(vimeo_request.data.decode('utf-8'))
-        video_thumb = data[0]['thumbnail_large']
+        vimeo_request = http.request(
+            "GET", "https://vimeo.com/api/v2/video/" + vimeo_id + ".json"
+        )
+        data = json.loads(vimeo_request.data.decode("utf-8"))
+        video_thumb = data[0]["thumbnail_large"]
     else:
         # the thumbs parameter is True, but the APOD for the date is not a video, output nothing
         video_thumb = ""
 
     return video_thumb
 
+
 # function that returns only last URL if there are multiple URLs stacked together
 def _get_last_url(data):
     regex = re.compile("(?:.(?!http[s]?://))+$")
     return regex.findall(data)[0]
 
+
 def _get_apod_chars(dt, thumbs):
-    media_type = 'image'
+    media_type = "image"
     if dt:
-        date_str = dt.strftime('%y%m%d')
-        apod_url = '%sap%s.html' % (BASE, date_str)
+        date_str = dt.strftime("%y%m%d")
+        apod_url = "%sap%s.html" % (BASE, date_str)
     else:
-        apod_url = '%sastropix.html' % BASE
-    LOG.debug('OPENING URL:' + apod_url)
+        apod_url = "%sastropix.html" % BASE
+    LOG.debug("OPENING URL:" + apod_url)
     res = requests.get(apod_url)
-    
+
     if res.status_code == 404:
         return None
         # LOG.error(f'No APOD entry for URL: {apod_url}')
@@ -75,49 +88,49 @@ def _get_apod_chars(dt, thumbs):
 
         # return default_obj_props
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    LOG.debug('getting the data url')
+    soup = BeautifulSoup(res.text, "html.parser")
+    LOG.debug("getting the data url")
     hd_data = None
     if soup.img:
         # it is an image, so get both the low- and high-resolution data
-        data = BASE + soup.img['src']
+        data = BASE + soup.img["src"]
         hd_data = data
 
-        LOG.debug('getting the link for hd_data')
-        for link in soup.find_all('a', href=True):
-            if link['href'] and link['href'].startswith('image'):
-                hd_data = BASE + link['href']
+        LOG.debug("getting the link for hd_data")
+        for link in soup.find_all("a", href=True):
+            if link["href"] and link["href"].startswith("image"):
+                hd_data = BASE + link["href"]
                 break
     elif soup.iframe:
         # its a video
-        media_type = 'video'
-        data = soup.iframe['src']
+        media_type = "video"
+        data = soup.iframe["src"]
     else:
         # it is neither image nor video, output empty urls
-        media_type = 'other'
-        data = ''
+        media_type = "other"
+        data = ""
 
     props = {}
 
-    props['explanation'] = _explanation(soup)
-    props['title'] = _title(soup)
+    props["explanation"] = _explanation(soup)
+    props["title"] = _title(soup)
     copyright_text = _copyright(soup)
     if copyright_text:
-        props['copyright'] = copyright_text
-    props['media_type'] = media_type
+        props["copyright"] = copyright_text
+    props["media_type"] = media_type
     if data:
-        props['url'] = _get_last_url(data)
+        props["url"] = _get_last_url(data)
     if dt:
-        props['date'] = dt.strftime('%Y-%m-%d')
+        props["date"] = dt.strftime("%Y-%m-%d")
     else:
-        props['date'] = _date(soup)
+        props["date"] = _date(soup)
 
     if hd_data:
-        props['hdurl'] = _get_last_url(hd_data)
+        props["hdurl"] = _get_last_url(hd_data)
 
     if thumbs and media_type == "video":
         if thumbs.lower() == "true":
-            props['thumbnail_url'] = _get_thumbs(data)
+            props["thumbnail_url"] = _get_thumbs(data)
 
     return props
 
@@ -128,34 +141,34 @@ def _title(soup):
     APOD image title.  Highly idiosyncratic with adaptations for different
     HTML structures that appear over time.
     """
-    LOG.debug('getting the title')
+    LOG.debug("getting the title")
     try:
         # Handler for later APOD entries
-        number_of_center_elements = len(soup.find_all('center'))
-        if(number_of_center_elements == 2):
-            center_selection = soup.find_all('center')[0]
-            bold_selection = center_selection.find_all('b')[0]
-            title = bold_selection.text.strip(' ')
+        number_of_center_elements = len(soup.find_all("center"))
+        if number_of_center_elements == 2:
+            center_selection = soup.find_all("center")[0]
+            bold_selection = center_selection.find_all("b")[0]
+            title = bold_selection.text.strip(" ")
             try:
-                title = title.encode('latin1').decode('cp1252')
+                title = title.encode("latin1").decode("cp1252")
             except Exception as ex:
                 LOG.error(str(ex))
         else:
-            center_selection = soup.find_all('center')[1]
-            bold_selection = center_selection.find_all('b')[0]
-            title = bold_selection.text.strip(' ')
+            center_selection = soup.find_all("center")[1]
+            bold_selection = center_selection.find_all("b")[0]
+            title = bold_selection.text.strip(" ")
             try:
-                title = title.encode('latin1').decode('cp1252')
+                title = title.encode("latin1").decode("cp1252")
             except Exception as ex:
                 LOG.error(str(ex))
-        
+
         return title
     except Exception:
         # Handler for early APOD entries
-        text = soup.title.text.split(' - ')[-1]
+        text = soup.title.text.split(" - ")[-1]
         title = text.strip()
         try:
-            title = title.encode('latin1').decode('cp1252')
+            title = title.encode("latin1").decode("cp1252")
         except Exception as ex:
             LOG.error(str(ex))
 
@@ -168,7 +181,7 @@ def _copyright(soup):
     APOD image copyright.  Highly idiosyncratic with adaptations for different
     HTML structures that appear over time.
     """
-    LOG.debug('getting the copyright')
+    LOG.debug("getting the copyright")
     try:
         # Handler for later APOD entries
         # There's no uniform handling of copyright (sigh). Well, we just have to
@@ -176,23 +189,22 @@ def _copyright(soup):
 
         copyright_text = None
         use_next = False
-        for element in soup.findAll('a', text=True):
+        for element in soup.findAll("a", text=True):
             # LOG.debug("TEXT: "+element.text)
 
             if use_next:
-                copyright_text = element.text.strip(' ')
+                copyright_text = element.text.strip(" ")
                 break
 
-            if 'Copyright' in element.text:
-                LOG.debug('Found Copyright text:' + str(element.text))
+            if "Copyright" in element.text:
+                LOG.debug("Found Copyright text:" + str(element.text))
                 use_next = True
 
         if not copyright_text:
-
-            for element in soup.findAll(['b', 'a'], text=True):
+            for element in soup.findAll(["b", "a"], text=True):
                 # search text for explicit match
-                if 'Copyright' in element.text:
-                    LOG.debug('Found Copyright text:' + str(element.text))
+                if "Copyright" in element.text:
+                    LOG.debug("Found Copyright text:" + str(element.text))
                     # pull the copyright from the link text which follows
                     sibling = element.next_sibling
                     stuff = ""
@@ -204,9 +216,9 @@ def _copyright(soup):
                         sibling = sibling.next_sibling
 
                     if stuff:
-                        copyright_text = stuff.strip(' ')
+                        copyright_text = stuff.strip(" ")
         try:
-            copyright_text = copyright_text.encode('latin1').decode('cp1252')
+            copyright_text = copyright_text.encode("latin1").decode("cp1252")
         except Exception as ex:
             LOG.error(str(ex))
 
@@ -214,7 +226,7 @@ def _copyright(soup):
 
     except Exception as ex:
         LOG.error(str(ex))
-        raise ValueError('Unsupported schema for given date.')
+        raise ValueError("Unsupported schema for given date.")
 
 
 def _explanation(soup):
@@ -223,18 +235,18 @@ def _explanation(soup):
     APOD image explanation.  Highly idiosyncratic.
     """
     # Handler for later APOD entries
-    LOG.debug('getting the explanation')
-    s = soup.find_all('p')[2].text
-    s = s.replace('\n', ' ')
-    s = s.replace('  ', ' ')
-    s = s.strip(' ').strip('Explanation: ')
-    s = s.split(' Tomorrow\'s picture')[0]
-    s = s.strip(' ')
-    if s == '':
+    LOG.debug("getting the explanation")
+    s = soup.find_all("p")[2].text
+    s = s.replace("\n", " ")
+    s = s.replace("  ", " ")
+    s = s.strip(" ").strip("Explanation: ")
+    s = s.split(" Tomorrow's picture")[0]
+    s = s.strip(" ")
+    if s == "":
         # Handler for earlier APOD entries
-        texts = [x.strip() for x in soup.text.split('\n')]
+        texts = [x.strip() for x in soup.text.split("\n")]
         try:
-            begin_idx = texts.index('Explanation:') + 1
+            begin_idx = texts.index("Explanation:") + 1
         except ValueError as e:
             # Rare case where "Explanation:" is not on its own line
             explanation_line = [x for x in texts if "Explanation:" in x]
@@ -244,11 +256,11 @@ def _explanation(soup):
             else:
                 raise e
 
-        idx = texts[begin_idx:].index('')
-        s = ' '.join(texts[begin_idx:begin_idx + idx])
+        idx = texts[begin_idx:].index("")
+        s = " ".join(texts[begin_idx : begin_idx + idx])
 
     try:
-        s = s.encode('latin1').decode('cp1252')
+        s = s.encode("latin1").decode("cp1252")
     except Exception as ex:
         LOG.error(str(ex))
 
@@ -260,29 +272,41 @@ def _date(soup):
     Accepts a BeautifulSoup object for the APOD HTML page and returns the
     date of the APOD image.
     """
-    LOG.debug('getting the date from soup data.')
+    LOG.debug("getting the date from soup data.")
     _today = datetime.date.today()
-    for line in soup.text.split('\n'):
+    for line in soup.text.split("\n"):
         today_year = str(_today.year)
-        yesterday_year = str((_today-datetime.timedelta(days=1)).year)
+        yesterday_year = str((_today - datetime.timedelta(days=1)).year)
         # Looks for the first line that starts with the current year.
         # This also checks yesterday's year so it doesn't break on January 1st at 00:00 UTC
         # before apod.nasa.gov uploads a new image.
         if line.startswith(today_year) or line.startswith(yesterday_year):
-            LOG.debug('found possible date match: ' + line)
+            LOG.debug("found possible date match: " + line)
             # takes apart the date string and turns it into a datetime
             try:
                 year, month, day = line.split()
                 year = int(year)
-                month = ['january', 'february', 'march', 'april',
-                         'may', 'june', 'july', 'august',
-                         'september', 'october', 'november', 'december'
-                         ].index(month.lower()) + 1
+                month = [
+                    "january",
+                    "february",
+                    "march",
+                    "april",
+                    "may",
+                    "june",
+                    "july",
+                    "august",
+                    "september",
+                    "october",
+                    "november",
+                    "december",
+                ].index(month.lower()) + 1
                 day = int(day)
-                return datetime.date(year=year, month=month, day=day).strftime('%Y-%m-%d')
+                return datetime.date(year=year, month=month, day=day).strftime(
+                    "%Y-%m-%d"
+                )
             except:
-                LOG.debug('unable to retrieve date from line: ' + line)
-    raise Exception('Date not found in soup data.')
+                LOG.debug("unable to retrieve date from line: " + line)
+    raise Exception("Date not found in soup data.")
 
 
 def parse_apod(dt, use_default_today_date=False, thumbs=False):
@@ -291,13 +315,12 @@ def parse_apod(dt, use_default_today_date=False, thumbs=False):
     of that day, noting that
     """
 
-    LOG.debug('apod chars called date:' + str(dt))
+    LOG.debug("apod chars called date:" + str(dt))
 
     try:
         return _get_apod_chars(dt, thumbs)
 
     except Exception as ex:
-
         # handle edge case where the service local time
         # miss-matches with 'todays date' of the underlying APOD
         # service (can happen because they are deployed in different
@@ -318,19 +341,14 @@ def get_concepts(request, text, apikey):
     Returns the concepts associated with the text, interleaved with integer
     keys indicating the index.
     """
-    cbase = 'http://access.alchemyapi.com/calls/text/TextGetRankedConcepts'
+    cbase = "http://access.alchemyapi.com/calls/text/TextGetRankedConcepts"
 
-    params = dict(
-        outputMode='json',
-        apikey=apikey,
-        text=text
-    )
+    params = dict(outputMode="json", apikey=apikey, text=text)
 
     try:
-
-        LOG.debug('Getting response')
+        LOG.debug("Getting response")
         response = json.loads(request.get(cbase, fields=params))
-        clist = [concept['text'] for concept in response['concepts']]
+        clist = [concept["text"] for concept in response["concepts"]]
         return {k: v for k, v in zip(range(len(clist)), clist)}
 
     except Exception as ex:
